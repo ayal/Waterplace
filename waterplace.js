ac7800f200b5fce9f370d11d7089879e2fb6d7e7
ayalgelles <ayal.gelles@simplified-networks.com>
2013-04-14 08:53:00 +0000

    chat almost there

--- waterplace.js
+++ waterplace.js
@@ -5,7 +5,17 @@
 // UI like forge
 // indexes?
 
-var Waterplace = new Meteor.Collection("waterplace");
+Waterplace = new Meteor.Collection("waterplace");
+
+Meteor.methods({
+  findone: function(selector) {
+    if (!this.isSimulation) {
+      var res = Waterplace.findOne(selector);
+      console.log('FINDONE METHOD CALL ON SERVER', res);
+      return JSON.stringify(res);
+    }
+  }
+})
 
 if (Meteor.isClient) {
     WP = function(url) {
@@ -14,17 +24,22 @@ if (Meteor.isClient) {
 	var parts = url.trim('/').split('/').reverse();
 	var lastpart = null;
 	$.each(parts, function(i, p){
-            parts[i] = parts.slice(i).reverse().join('/');
+     parts[i] = parts.slice(i).reverse().join('/');
 	});
 
 	$.each(parts, function(i, part){
-	    var kv = Waterplace.findOne({key: part});
+    // Meteor.call('findone', {key: part}, function(ex, kv){
+    // kv = kv ? JSON.parse(kv) : kv;
+    // });
+    
+    kv = Waterplace.findOne({key: part});
 	    var id = null;
 	    if (!kv) {
 		// add time
-		var values = {};
+    var values = {empty: 1};
 		var lastref = null;
 		if (lastpart) {
+      delete values.empty;
 		    values[lastpart.key] = lastpart.id;
 		}
 		if (part) {
@@ -33,37 +48,45 @@ if (Meteor.isClient) {
 	    }
 	    else {
 		id = kv._id;
-		if (lastpart) {
-		    var toupdate = {};
-		    toupdate['values.' + lastpart.key] = lastpart.id;
+		if (lastpart && kv.values[lastpart.key] !== lastpart.id) {
+      // TODO: grabage collection of overridden objects
+      var newvals = typeof kv.values !== 'object' ? {} : kv.values;
+      var toupdate = {values: newvals};
+		    toupdate.values[lastpart.key] = lastpart.id;
 		    Waterplace.update(kv._id, {$set: toupdate});
 		}
 	    }
 
-	    if (lastpart) {
-		Waterplace.update(lastpart._id, {$set: {parent: id}})
+	    if (lastpart && id) {
+		Waterplace.update(lastpart.id, {$set: {parent: id}})
 	    }
 
 	    lastpart = {id: id, key: part};
 	    if (!self._ref) {
 		self._ref = lastpart;
-		self.ref = function() {
-		    return Waterplace.findOne(self._ref.id);
-		};
+	
 	    }
 	});
-
-	var objectify = function(x) {
+      
+	self.ref = function() {
+		    return self;
+  };
+      
+   self.name = function(){
+     return self._ref.key;
+   }
+      
+	this.objectify = function(x) {
 	    if (!x) {
-		return null;
+		     return null;
 	    }
 
 	    if (typeof x.values == undefined || x.values === null) {
-                return null;
-            }
+         return null;
+      }
 
 	    if (!x.key) {
-		return x.value;
+		    return x.value;
 	    }
 
 	    var obj = {};
@@ -71,13 +94,13 @@ if (Meteor.isClient) {
 
 
 	    if (typeof x.values !== 'object') {
-		parent[x.key.split('/').splice(-1)[0]] = x.values;
+		     parent[x.key.split('/').splice(-1)[0]] = x.values;
 	    }
 	    else {
-		parent[x.key.split('/').splice(-1)[0]] = obj;
-		$.each(x.values, function(k,v) {
+		       parent[x.key.split('/').splice(-1)[0]] = obj;
+		       $.each(x.values, function(k,v) {
 		    var key = k.split('/').splice(-1)[0];
-		    $.extend(obj, objectify(Waterplace.findOne(v))); 
+		    $.extend(obj, self.objectify(Waterplace.findOne(v))); 
 		});
 	    }
 	    
@@ -85,35 +108,44 @@ if (Meteor.isClient) {
 	};
 
 	this.val = function(){
-	    return objectify(self.ref());
+	    return _.values(self.objectify(Waterplace.findOne(self._ref.id)))[0];
 	}
 
 	this.set = function(obj) {
 	    if (typeof obj !== 'object') {
-		Waterplace.update(self._ref.id, {$set: {values: obj}});
-		return self;
+		     Waterplace.update(self._ref.id, {$set: {values: obj}});
+		     return self;
 	    }
 
 	    $.each(obj, function(k,v) {
-		var n = new WP(self._ref.key + '/' + k);
-		n.set(v);
+		     var n = new WP(self._ref.key + '/' + k);
+		     n.set(v);
 	    });
 	    return self;
 	};
 
-	this.push = function(){
-	    // TODO: add type array with 0,1,2 as keys and special objectify treatment
+	this.push = function(v) {
+    var objtoset = {};
+    objtoset['' + (new Date()).getTime()] = v;
+	  self.set(objtoset);
 	}
+  
+  this.once = function(what, cb) {
+      self.on(what,cb,true);
+    };
 
-	this.on = function(what, cb) {
+	this.on = function(what, cb, stop) {
+    // use "stop" to create a "once" effect
+    
 	    var debouncedcb = _.debounce(cb, 0);
 
 	    Waterplace.find().observe({
-		added: function(obj) {
+		  added: function(obj) {
 		    // triggered twice if client inserts?
 		    var ref = self.ref();
-		    if (obj && obj.key && obj.key.indexOf(ref.key) === 0 && 
-			obj.key.split('/').length - 1 === ref.key.split('/').length) {
+        
+		    if (obj && obj.key && obj.key.indexOf(self._ref.key) === 0 && 
+			obj.key.split('/').length - 1 === self._ref.key.split('/').length) {
 			setTimeout(function(){
 			    what === 'child_added' && cb(new WP(obj.key));
 			},0);
@@ -122,44 +154,21 @@ if (Meteor.isClient) {
 		changed: function(obj, old) {
 		    // no old object
 		    var ref = self.ref();
-		    if (obj &&  obj.key && obj.key.indexOf(ref.key) === 0) {
+		    if (obj &&  obj.key && obj.key.indexOf(self._ref.key) === 0) {
 			setTimeout(function(){
-			    what === 'value' && debouncedcb(new WP(ref.key));
+			    what === 'value' && debouncedcb(new WP(self._ref.key));
 			})
 		    }
 		}
 	    });	
 	};
     }
+    
+    
 
 
-    // tests:
-
-    test = function(){
-
-
-	var ayalast = new WP('/users/' + new Date().getTime() + '-ayal/name/last');	
-	var amirlast = new WP('/users/' + new Date().getTime() + '-amir/name/last');
-	amirlast.on('value', function(c){
-	    console.log('amirlast changed', JSON.stringify(c.val()));
-	    console.log(Waterplace.find().fetch());
-	});
-
-	ayalast.set('gelles');
-	amirlast.set({thename: {is: {gafner: true}}});
-    }
-
-    Meteor.startup(function() {
-	var users = new WP('/users');
-	users.on('child_added', function(user){
-	    console.log('child added', user.val());
-	});
-
-	users.on('value', function(user){
-	    console.log('changed', user.val());
-	});
-
-    });
+  Meteor.startup(function() {
+   });
 
 }
 
